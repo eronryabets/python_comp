dafb0233455a4672c60df64df303a90088698717
eronryabets <eronryabets@gmail.com>
2017-04-04 01:15:40 +0300

    upload v1

--- test.py
+++ test.py
@@ -13,6 +13,20 @@ print(int(sum_1) - int(sum_2))
 """
 
 """(Calc +) v1.1 """
+"""
 sum_1 = int(input ('Please, enter a first number ' ))
 sum_2 = int(input ('Please, enter a Second number ' ))
 print(sum_1 + sum_2)
+"""
+
+#pay = 22
+#print(str(33) + str(pay))
+
+""" Calc of salary """
+
+hours = int(input ('Please, enter your work hours ' ))
+rate = int(input ('Please, enter your rate ' ))
+#pay = hours * rate # Wrong!
+pay = str(hours * rate)
+#print(pay)
+print('Your pay is ' + pay)
